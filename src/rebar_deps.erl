kellymclaughlin/rebar

commit a26acb2f09e1f2a3b2b98374a7b3c2c155f6426d

Bug fixes and improvements for deps handling
Allow deps to specify a force_version option to indicate that
subsequent deps specifications for the same application should be
ignored. This is helpful for a number of situations such as being able
to test the interaction of different versions of a subset of deps
without having to do a lot of work to edit nested rebar.config
files. This change set also resolves a few bugs dealing with vcs
version determination and does some refactoring of enormous functions
to at least be slightly smaller.

src/rebar_deps.erl
@@ -45,6 +45,7 @@
                app,
                vsn_regex,
                source,
+               force_version, %% Ignore other versions of same app
                is_raw }). %% is_raw = true means non-Erlang/OTP dependency
 
 %% ===================================================================
@@ -62,11 +63,13 @@ preprocess(Config, _) ->
     Deps = rebar_config:get_local(Config1, deps, []),
     {Config2, {AvailableDeps, MissingDeps}} = find_deps(Config1, find, Deps),
 
-    ?DEBUG("Available deps: ~p\n", [AvailableDeps]),
-    ?DEBUG("Missing deps  : ~p\n", [MissingDeps]),
+    %% Update the set of deps that are have `force_version' specified to indicate
+    %% the specified version should be used regardless of any subsequent versions
+    %% of the same app that are encountered.
+    Config3 = maybe_update_force_deps(AvailableDeps ++ MissingDeps, Config2),
 
     %% Add available deps to code path
-    Config3 = update_deps_code_path(Config2, AvailableDeps),
+    Config4 = update_deps_code_path(Config3, AvailableDeps),
 
     %% If skip_deps=true, mark each dep dir as a skip_dir w/ the core so that
     %% the current command doesn't run on the dep dir. However, pre/postprocess
@@ -74,12 +77,12 @@ preprocess(Config, _) ->
     %%
     %% Also, if skip_deps=comma,separated,app,list, then only the given
     %% dependencies are skipped.
-    NewConfig = case rebar_config:get_global(Config3, skip_deps, false) of
+    NewConfig = case rebar_config:get_global(Config4, skip_deps, false) of
         "true" ->
             lists:foldl(
                 fun(#dep{dir = Dir}, C) ->
                         rebar_config:set_skip_dir(C, Dir)
-                end, Config3, AvailableDeps);
+                end, Config4, AvailableDeps);
         Apps when is_list(Apps) ->
             SkipApps = [list_to_atom(App) || App <- string:tokens(Apps, ",")],
             lists:foldl(
@@ -88,9 +91,9 @@ preprocess(Config, _) ->
                             true -> rebar_config:set_skip_dir(C, Dir);
                             false -> C
                         end
-                end, Config3, AvailableDeps);
+                end, Config4, AvailableDeps);
         _ ->
-            Config3
+            Config4
     end,
 
     %% Filtering out 'raw' dependencies so that no commands other than
@@ -156,14 +159,12 @@ do_check_deps(Config) ->
     Deps = rebar_config:get_local(Config, deps, []),
     {Config1, {_AvailableDeps, MissingDeps}} = find_deps(Config, find, Deps),
     MissingDeps1 = [D || D <- MissingDeps, D#dep.source =/= undefined],
-
     %% For each missing dep with a specified source, try to pull it.
     {Config2, PulledDeps} =
         lists:foldl(fun(D, {C, PulledDeps0}) ->
                             {C1, D1} = use_source(C, D),
                             {C1, [D1 | PulledDeps0]}
                     end, {Config1, []}, MissingDeps1),
-
     %% Add each pulled dep to our list of dirs for post-processing. This yields
     %% the necessary transitivity of the deps
     {ok, save_dep_dirs(Config2, lists:reverse(PulledDeps))}.
@@ -197,7 +198,7 @@ do_check_deps(Config) ->
         {Config1, {AvailDeps, []}} ->
             lists:foreach(fun(Dep) -> print_source(Dep) end, AvailDeps),
             {ok, save_dep_dirs(Config1, AvailDeps)};
-        {_, MissingDeps} ->
+        {_, {_, MissingDeps}} ->
             ?ABORT("Missing dependencies: ~p\n", [MissingDeps])
     end.
 
@@ -279,8 +280,7 @@ update_deps_code_path(Config, []) ->
     Config;
 update_deps_code_path(Config, [Dep | Rest]) ->
     Config2 =
-        case is_app_available(Config, Dep#dep.app,
-                              Dep#dep.vsn_regex, Dep#dep.dir, Dep#dep.is_raw) of
+        case is_app_available(Config, Dep) of
             {Config1, {true, _}} ->
                 Dir = filename:join(Dep#dep.dir, "ebin"),
                 ok = filelib:ensure_dir(filename:join(Dir, "dummy")),
@@ -311,6 +311,7 @@ find_deps(Config, Mode, [{App, VsnRegex, Source, Opts} | Rest], Acc) when is_lis
     Dep = #dep { app = App,
                  vsn_regex = VsnRegex,
                  source = Source,
+                 force_version = lists:member(force_version, Opts),
                  %% dependency is considered raw (i.e. non-Erlang/OTP) when
                  %% 'raw' option is present
                  is_raw = proplists:get_value(raw, Opts, false) },
@@ -346,10 +347,7 @@ find_dep(Config, Dep, _Source) ->
 find_dep_in_dir(Config, _Dep, {false, Dir}) ->
     {Config, {missing, Dir}};
 find_dep_in_dir(Config, Dep, {true, Dir}) ->
-    App = Dep#dep.app,
-    VsnRegex = Dep#dep.vsn_regex,
-    IsRaw = Dep#dep.is_raw,
-    case is_app_available(Config, App, VsnRegex, Dir, IsRaw) of
+    case is_app_available(Config, Dep#dep{dir=Dir}) of
         {Config1, {true, _AppFile}} -> {Config1, {avail, Dir}};
         {Config1, {false, _}}       -> {Config1, {missing, Dir}}
     end.
@@ -378,7 +376,10 @@ require_source_engine(Source) ->
 %%
 %% IsRaw = true means non-Erlang/OTP dependency, e.g. the one that does not
 %% have a proper .app file
-is_app_available(Config, App, VsnRegex, Path, _IsRaw = false) ->
+is_app_available(Config, Dep=#dep{is_raw=IsRaw}) when IsRaw =:= false ->
+    #dep{app=App,
+         vsn_regex=VsnRegex,
+         dir=Path} = Dep,
     ?DEBUG("is_app_available, looking for App ~p with Path ~p~n", [App, Path]),
     case rebar_app_utils:is_app_dir(Path) of
         {true, AppFile} ->
@@ -391,12 +392,17 @@ is_app_available(Config, App, VsnRegex, Path, _IsRaw = false) ->
                         match ->
                             {Config2, {true, Path}};
                         nomatch ->
-                            ?WARN("~s has version ~p; requested regex was ~s\n",
-                                  [AppFile, Vsn, VsnRegex]),
-                            {Config2,
-                             {false, {version_mismatch,
-                                      {AppFile,
-                                       {expected, VsnRegex}, {has, Vsn}}}}}
+                            case is_force_dep(App, Config) of
+                                true ->
+                                    {Config2, {true, Path}};
+                                false ->
+                                    ?WARN("~s has version ~p; requested regex was ~s\n",
+                                          [AppFile, Vsn, VsnRegex]),
+                                    {Config2,
+                                     {false, {version_mismatch,
+                                              {AppFile,
+                                               {expected, VsnRegex}, {has, Vsn}}}}}
+                            end
                     end;
                 {Config1, OtherApp} ->
                     ?WARN("~s has application id ~p; expected ~p\n",
@@ -410,7 +416,8 @@ is_app_available(Config, App, VsnRegex, Path, _IsRaw = false) ->
                   "but no .app found.\n", [Path]),
             {Config, {false, {missing_app_file, Path}}}
     end;
-is_app_available(Config, App, _VsnRegex, Path, _IsRaw = true) ->
+is_app_available(Config, Dep=#dep{is_raw=IsRaw}) when IsRaw =:= true ->
+    #dep{app=App, dir=Path} = Dep,
     ?DEBUG("is_app_available, looking for Raw Depencency ~p with Path ~p~n", [App, Path]),
     case filelib:is_dir(Path) of
         true ->
@@ -434,8 +441,7 @@ use_source(Config, Dep, Count) ->
     case filelib:is_dir(Dep#dep.dir) of
         true ->
             %% Already downloaded -- verify the versioning matches the regex
-            case is_app_available(Config, Dep#dep.app,
-                                  Dep#dep.vsn_regex, Dep#dep.dir, Dep#dep.is_raw) of
+            case is_app_available(Config, Dep) of
                 {Config1, {true, _}} ->
                     Dir = filename:join(Dep#dep.dir, "ebin"),
                     ok = filelib:ensure_dir(filename:join(Dir, "dummy")),
@@ -456,6 +462,23 @@ use_source(Config, Dep, Count) ->
             download_source(TargetDir, Dep#dep.source),
             use_source(Config, Dep#dep { dir = TargetDir }, Count-1)
     end.
+
+is_force_dep(App, Config) ->
+    ForceDeps = rebar_config:get_xconf(Config, force_deps),
+    ordsets:is_element(App, ForceDeps).
+
+maybe_update_force_deps([], Config) ->
+    Config;
+maybe_update_force_deps([#dep{app=App, force_version=true} | RestDeps],
+                        Config) ->
+    ForceDeps = rebar_config:get_xconf(Config, force_deps),
+    maybe_update_force_deps(
+      RestDeps,
+      rebar_config:set_xconf(Config,
+                             force_deps,
+                             ordsets:add_element(App, ForceDeps)));
+maybe_update_force_deps([#dep{force_version=false} | RestDeps], Config) ->
+    maybe_update_force_deps(RestDeps, Config).
 
 download_source(AppDir, {hg, Url, Rev}) ->
     ok = filelib:ensure_dir(AppDir),
@@ -630,6 +653,8 @@ has_vcs_dir(rsync, _) ->
 has_vcs_dir(_, _) ->
     true.
 
+print_source(#dep{app=App, source=Source, force_version=true}) ->
+    ?CONSOLE("~s (Forced Version)~n", [format_source(App, Source)]);
 print_source(#dep{app=App, source=Source}) ->
     ?CONSOLE("~s~n", [format_source(App, Source)]).
 

src/rebar.erl
@@ -146,7 +146,8 @@ init_config({Options, _NonOptArgs}) ->
     %% Keep track of how many operations we do, so we can detect bad commands
     BaseConfig1 = rebar_config:set_xconf(BaseConfig, operations, 0),
     %% Initialize vsn cache
-    rebar_config:set_xconf(BaseConfig1, vsn_cache, dict:new()).
+    BaseConfig2 = rebar_config:set_xconf(BaseConfig1, vsn_cache, dict:new()),
+    rebar_config:set_xconf(BaseConfig2, force_deps, ordsets:new()).
 
 init_config1(BaseConfig) ->
     %% Determine the location of the rebar executable; important for pulling

src/rebar_app_utils.erl
@@ -97,10 +97,9 @@ app_applications(Config, AppFile) ->
 
 app_vsn(Config, AppFile) ->
     case load_app_file(Config, AppFile) of
-        {ok, Config1, _, AppInfo} ->
+        {ok, Config1, AppName, _AppInfo} ->
             AppDir = filename:dirname(filename:dirname(AppFile)),
-            rebar_utils:vcs_vsn(Config1, get_value(vsn, AppInfo, AppFile),
-                                AppDir);
+            rebar_utils:vcs_vsn(Config1, AppName, AppDir);
         {error, Reason} ->
             ?ABORT("Failed to extract vsn from ~s: ~p\n",
                    [AppFile, Reason])

src/rebar_core.erl
@@ -172,8 +172,36 @@ skip_or_process_dir1(AppFile, ModuleSet, Config, CurrentCodePath,
                          CurrentCodePath, ModuleSet)
     end.
 
-process_dir1(Dir, Command, DirSet, Config0, CurrentCodePath,
-             {DirModules, ModuleSetFile}) ->
+-spec process_current_dir(atom(), rebar_config:config(), [atom()], [atom()],
+                          set(), string(), boolean()) ->
+                                 rebar_config:config().
+process_current_dir(Command, Config, _, _, _, Dir, true) ->
+    %% Do not execute the command on the directory, as some
+    %% module has requested a skip on it.
+    ?INFO("Skipping ~s in ~s\n", [Command, Dir]),
+    Config;
+process_current_dir(Command, Config, Modules, PluginModules,
+                    ModuleSetFile, Dir, false) ->
+    %% Make sure the CWD is reset properly; processing the dirs may have
+    %% caused it to change
+    ok = file:set_cwd(Dir),
+
+    %% Check for and get command specific environments
+    {Config2, Env} = setup_envs(Config, Modules),
+
+    %% Execute any before_command plugins on this directory
+    Config3 = execute_pre(Command, PluginModules,
+                          Config2, ModuleSetFile, Env),
+
+    %% Execute the current command on this directory
+    Config4 = execute(Command, Modules ++ PluginModules,
+                      Config3, ModuleSetFile, Env),
+
+    %% Execute any after_command plugins on this directory
+    execute_post(Command, PluginModules,
+                 Config4, ModuleSetFile, Env).
+
+process_dir_prepare(Config, Dir, DirModules, ModuleSetFile) ->
     %% Get the list of modules for "any dir". This is a catch-all list
     %% of modules that are processed in addition to modules associated
     %% with this directory type. These any_dir modules are processed
@@ -184,7 +212,7 @@ process_dir1(Dir, Command, DirSet, Config0, CurrentCodePath,
 
     %% Invoke 'preprocess' on the modules -- this yields a list of other
     %% directories that should be processed _before_ the current one.
-    {Config1, Predirs} = acc_modules(Modules, preprocess, Config0,
+    {Config1, Predirs} = acc_modules(Modules, preprocess, Config,
                                      ModuleSetFile),
 
     %% Remember associated pre-dirs (used for plugin lookup)
@@ -197,62 +225,54 @@ process_dir1(Dir, Command, DirSet, Config0, CurrentCodePath,
     {Config2, PluginPredirs} = acc_modules(PluginModules, preprocess,
                                            Config1, ModuleSetFile),
 
-    AllPredirs = Predirs ++ PluginPredirs,
+    {Config2, Modules, PluginModules, Predirs ++ PluginPredirs}.
 
-    ?DEBUG("Predirs: ~p\n", [AllPredirs]),
-    {Config3, DirSet2} = process_each(AllPredirs, Command, Config2,
-                                      ModuleSetFile, DirSet),
-
-    %% Make sure the CWD is reset properly; processing the dirs may have
-    %% caused it to change
-    ok = file:set_cwd(Dir),
-
-    %% Check that this directory is not on the skip list
-    Config7 = case rebar_config:is_skip_dir(Config3, Dir) of
-                  true ->
-                      %% Do not execute the command on the directory, as some
-                      %% module has requested a skip on it.
-                      ?INFO("Skipping ~s in ~s\n", [Command, Dir]),
-                      Config3;
-
-                  false ->
-                      %% Check for and get command specific environments
-                      {Config4, Env} = setup_envs(Config3, Modules),
-
-                      %% Execute any before_command plugins on this directory
-                      Config5 = execute_pre(Command, PluginModules,
-                                            Config4, ModuleSetFile, Env),
-
-                      %% Execute the current command on this directory
-                      Config6 = execute(Command, Modules ++ PluginModules,
-                                        Config5, ModuleSetFile, Env),
-
-                      %% Execute any after_command plugins on this directory
-                      execute_post(Command, PluginModules,
-                                   Config6, ModuleSetFile, Env)
-              end,
-
-    %% Mark the current directory as processed
-    DirSet3 = sets:add_element(Dir, DirSet2),
-
+post_process_dir(Command, Config, DirSet, Modules, ModuleSetFile) ->
     %% Invoke 'postprocess' on the modules. This yields a list of other
     %% directories that should be processed _after_ the current one.
-    {Config8, Postdirs} = acc_modules(Modules ++ PluginModules, postprocess,
-                                      Config7, ModuleSetFile),
+    {Config1, Postdirs} =
+        acc_modules(Modules, postprocess, Config, ModuleSetFile),
     ?DEBUG("Postdirs: ~p\n", [Postdirs]),
-    Res = process_each(Postdirs, Command, Config8,
-                       ModuleSetFile, DirSet3),
+    process_each(Postdirs, Command, Config1, ModuleSetFile, DirSet).
 
+process_dir1(Dir, Command, DirSet, Config, CurrentCodePath,
+             {DirModules, ModuleSetFile}) ->
+    {Config1, Modules, PluginModules, AllPredirs} =
+        process_dir_prepare(Config, Dir, DirModules, ModuleSetFile),
+
+    ?DEBUG("Predirs: ~p\n", [AllPredirs]),
+    {Config2, DirSet1} = process_each(AllPredirs, Command, Config1,
+                                      ModuleSetFile, DirSet),
+    Config3 = process_current_dir(Command,
+                                  Config2,
+                                  Modules,
+                                  PluginModules,
+                                  ModuleSetFile,
+                                  Dir,
+                                  rebar_config:is_skip_dir(Config2, Dir)),
+
+    %% Mark the current directory as processed
+    DirSet2 = sets:add_element(Dir, DirSet1),
+
+    Res = post_process_dir(Command,
+                           Config3,
+                           DirSet2,
+                           Modules ++ PluginModules,
+                           ModuleSetFile),
+
+    process_dir_cleanup(Dir, CurrentCodePath),
+
+    %% Return the updated {config, dirset} as result
+    Res.
+
+process_dir_cleanup(Dir, CodePath) ->
     %% Make sure the CWD is reset properly; processing the dirs may have
     %% caused it to change
     ok = file:set_cwd(Dir),
 
     %% Once we're all done processing, reset the code path to whatever
     %% the parent initialized it to
-    restore_code_path(CurrentCodePath),
-
-    %% Return the updated {config, dirset} as result
-    Res.
+    restore_code_path(CodePath).
 
 remember_cwd_predirs(Cwd, Predirs) ->
     Store = fun(Dir, Dict) ->

src/rebar_utils.erl
@@ -199,7 +199,21 @@ expand_env_variable(InStr, VarName, RawVarValue) ->
             re:replace(InStr, RegEx, [VarValue, "\\2"], ReOpts)
     end.
 
-vcs_vsn(Config, Vcs, Dir) ->
+app_vcs(false) ->
+    unknown;
+app_vcs({_, _}) ->
+    unknown;
+app_vcs({_, _, {Vcs, _}}) ->
+    Vcs;
+app_vcs({_, _, {Vcs, _, _}}) ->
+    Vcs;
+app_vcs({_, _, {Vcs, _}, _}) ->
+    Vcs;
+app_vcs({_, _, {Vcs, _, _}, _}) ->
+    Vcs.
+
+vcs_vsn(Config, App, Dir) ->
+    Vcs = app_vcs(lists:keyfind(App, 1, rebar_config:get(Config, deps, []))),
     Key = {Vcs, Dir},
     Cache = rebar_config:get_xconf(Config, vsn_cache),
     case dict:find(Key, Cache) of

commit ea74fa9c977df56617ad3ec18dddedff8703c389

rebar_deps: fix minor issues introduced in a26acb2f
- restore meaningful blank lines
- is_app_avilable/2: use spaces in record syntax to match surrounding
  code
- use simpler is_raw #dep{} record field match
- fix minor typo

src/rebar_deps.erl
@@ -63,9 +63,9 @@ preprocess(Config, _) ->
     Deps = rebar_config:get_local(Config1, deps, []),
     {Config2, {AvailableDeps, MissingDeps}} = find_deps(Config1, find, Deps),
 
-    %% Update the set of deps that are have `force_version' specified to indicate
-    %% the specified version should be used regardless of any subsequent versions
-    %% of the same app that are encountered.
+    %% Update the set of deps that have `force_version' specified to indicate
+    %% the specified version should be used regardless of any subsequent
+    %% versions of the same app that are encountered.
     Config3 = maybe_update_force_deps(AvailableDeps ++ MissingDeps, Config2),
 
     %% Add available deps to code path
@@ -142,12 +142,14 @@ do_check_deps(Config) ->
     Deps = rebar_config:get_local(Config, deps, []),
     {Config1, {_AvailableDeps, MissingDeps}} = find_deps(Config, find, Deps),
     MissingDeps1 = [D || D <- MissingDeps, D#dep.source =/= undefined],
+
     %% For each missing dep with a specified source, try to pull it.
     {Config2, PulledDeps} =
         lists:foldl(fun(D, {C, PulledDeps0}) ->
                             {C1, D1} = use_source(C, D),
                             {C1, [D1 | PulledDeps0]}
                     end, {Config1, []}, MissingDeps1),
+
     %% Add each pulled dep to our list of dirs for post-processing. This yields
     %% the necessary transitivity of the deps
     {ok, save_dep_dirs(Config2, lists:reverse(PulledDeps))}.
@@ -375,14 +377,14 @@ require_source_engine(Source) ->
     true = source_engine_avail(Source),
     ok.
 
-%% IsRaw = false means regular Erlang/OTP dependency
+%% is_raw = false means regular Erlang/OTP dependency
 %%
-%% IsRaw = true means non-Erlang/OTP dependency, e.g. the one that does not
+%% is_raw = true means non-Erlang/OTP dependency, e.g. the one that does not
 %% have a proper .app file
-is_app_available(Config, Dep=#dep{is_raw=IsRaw}) when IsRaw =:= false ->
-    #dep{app=App,
-         vsn_regex=VsnRegex,
-         dir=Path} = Dep,
+is_app_available(Config, Dep=#dep{is_raw=false}) ->
+    #dep{ app=App,
+          vsn_regex=VsnRegex,
+          dir=Path } = Dep,
     ?DEBUG("is_app_available, looking for App ~p with Path ~p~n", [App, Path]),
     case rebar_app_utils:is_app_dir(Path) of
         {true, AppFile} ->
@@ -421,7 +423,7 @@ is_app_available(Config, Dep=#dep{is_raw=IsRaw}) when IsRaw =:= false ->
                   "but no .app found.\n", [Path]),
             {Config, {false, {missing_app_file, Path}}}
     end;
-is_app_available(Config, Dep=#dep{is_raw=IsRaw}) when IsRaw =:= true ->
+is_app_available(Config, Dep=#dep{is_raw=true}) ->
     #dep{app=App, dir=Path} = Dep,
     ?DEBUG("is_app_available, looking for Raw Depencency ~p with Path ~p~n", [App, Path]),
     case filelib:is_dir(Path) of